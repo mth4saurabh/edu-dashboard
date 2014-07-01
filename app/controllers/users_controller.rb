d865785c327e69076eb029bb5bbad4d32a5338fa
Saurabh <sbansod@iitk.ac.in>
2014-07-01 12:51:28 +0530

    added devise

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, :except => [:create]
+  def index
+    
+  end
+    
+
+  def show
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the AD IQ Dashboard !"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+# You can add backtrace silencers for libraries that you're using but don't wish to see in your backtraces.
+# Rails.backtrace_cleaner.add_silencer { |line| line =~ /my_noisy_library/ }
+
+# You can also remove all the silencers if you're trying to debug a problem that might stem from framework code.
+# Rails.backtrace_cleaner.remove_silencers!
+QuizDashboard::Application.config.secret_key_base = 'c976769ef69cd83fac59e9d2388ffae42ee2e19410ea0307fa36b652b18574cc373ceff5eaa9cc30a43c0a0939002267705b10e819f3e1966e3f64d528dc81c5'
